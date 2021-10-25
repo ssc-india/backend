3cd09b9c89cc28c05f64cf1a2023aa279e616ec3
niranjannkamath <niranjannkamath@gmail.com>
2021-10-25 09:44:49 +0530

    remove redundant condition to check for query parameters in /content/show route

--- src/routes/content/show.ts
+++ src/routes/content/show.ts
@@ -15,27 +15,25 @@ interface QueryParams {
 
 router.get('/content/show', async (req: Request, res: Response) => {
     let posts: ContentDoc[] = [];
-    if (Object.keys(req.query).length === 0) {
-       posts = await Content.find().populate('author');
-     } else {
-        const { id, institute, branch, tag } = req.query as QueryParams; 
-        const query: { [key: string]: string } = {};
-        if (id) {
-            if (mongoose.Types.ObjectId.isValid(id)) {
-                query['_id'] = id;
-            } else {
-                throw new BadRequestError('Invalid ID');
-            }
-        }
-          
-        if (institute) query['institute'] = institute;
-
-        if (branch) query['branch'] = branch;
         
-        if (tag) query['tag'] = tag;
-
-        posts = await Content.find(query).populate('author'); 
+    const { id, institute, branch, tag } = req.query as QueryParams; 
+    const query: { [key: string]: string } = {};
+    
+    if (id) {
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            query['_id'] = id;
+        } else {
+            throw new BadRequestError('Invalid ID');
+        }
     }
+      
+    if (institute) query['institute'] = institute;
+
+    if (branch) query['branch'] = branch;
+    
+    if (tag) query['tag'] = tag;
+
+    posts = await Content.find(query).populate('author'); 
      
     res.status(200).send({ posts });
 });
